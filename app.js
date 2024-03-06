0073eb98c9332a287dd56d81cf43c108968b66a0
krishnapratap singh thakur <krishnapratapsinghthakur7@gmail.com>
2024-03-06 12:06:04 +0530

    episode-1

---
+++ app.js
@@ -0,0 +1,30 @@
+const parennt=React.createElement("div",
+{id :"krishna"},
+React.createElement("div",{id :""},[React.createElement("h1",{id :""},"jai jai siya ramm!!!!",),React.createElement("h2",{id :""},"jai jai siya ramm!!!!",)]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const heading = React.createElement("h1" , { id :"heading"} ,"jai shree ram")
+
+    const root = ReactDOM.createRoot(document.getElementById("root"))
+
+    root.render(parennt)
